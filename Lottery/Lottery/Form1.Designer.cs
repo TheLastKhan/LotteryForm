4149bb2dbeb6c14446c997ff58dc6835ae9a04e1
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-12-14 23:28:38 +0300

    Add files via upload

---
+++ Lottery/Lottery/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Draw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Player Mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(139, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(90, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "System Mode";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(139, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Hakan";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(12, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 554);
+            this.panel1.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(255, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(75, 69);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(336, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(75, 69);
+            this.listBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(730, 657);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lottery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Lottery/Lottery/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private List<int> lottery = new List<int>();
+        private List<int> lucky = new List<int>();
+        private List<int> completedPlayers = new List<int>(); // Stores the indices of completed players
+        private Dictionary<int, string> playerNames = new Dictionary<int, string>(); // Stores player names
+        private Dictionary<int, int> playerMatches = new Dictionary<int, int>(); // Stores the match counts for players
+        bool button1Clicked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Happy New Year !!! :) :D");
+
+            comboBox1.Items.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                comboBox1.Items.Add(i + 1 + " Player");
+            }
+
+            for (int i = 1; i <= 50; i++)
+            {
+                lottery.Add(i);
+            }
+
+            panel1.AutoScroll = true; // Added scroll feature
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var choice = MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (choice == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Have a nice day!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+            button1Clicked = true;
+
+            // Perform error check
+            if (!ErrorCheck())
+            {
+                return;
+            }
+
+            // Handle game mode selection
+            if (checkBox1.Checked)
+            {
+                MessageBox.Show("Player mode selected...");
+            }
+            else if (checkBox2.Checked)
+            {
+                MessageBox.Show("System mode selected...");
+            }
+
+            // Get player count and initialize game
+            int playerCount = comboBox1.SelectedIndex + 1;
+
+            InitializeLotteryNumbers();
+            InitializeGameUI(playerCount);
+        }
+
+        private bool ErrorCheck()
+        {
+            // Check if both checkboxes are selected
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("You cannot select both options. Please choose only one...");
+                return false;
+            }
+
+            // Check if neither checkbox is selected
+            if (!checkBox1.Checked && !checkBox2.Checked)
+            {
+                MessageBox.Show("Please select at least one game mode...");
+                return false;
+            }
+
+            // Check if player count is selected
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select the number of players...");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private void InitializeLotteryNumbers()
+        {
+            if (lottery.Count == 0)
+            {
+                for (int i = 1; i <= 50; i++)
+                {
+                    lottery.Add(i);
+                }
+            }
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            if (listBox2.Items.Count == 0)
+            {
+                for (int i = 1; i <= 50; i++)
+                {
+                    listBox2.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void InitializeGameUI(int playerCount)
+        {
+            string formattedLotteryNumbers = "";
+            for (int i = 0; i < lottery.Count; i++)
+            {
+                formattedLotteryNumbers += lottery[i] + " ";
+
+                if ((i + 1) % 10 == 0 || i == lottery.Count - 1)
+                {
+                    formattedLotteryNumbers += "\n";
+                }
+            }
+
+            panel1.Controls.Clear();
+            MessageBox.Show("New game started!");
+
+            int playersPerColumn = 5; // Maximum of 5 players per column
+            CreatePlayerPanels(playerCount, playersPerColumn);
+        }
+
+        private void CreatePlayerPanels(int playerCount, int playersPerColumn)
+        {
+            for (int playerIndex = 0; playerIndex < playerCount; playerIndex++)
+            {
+                Panel playerPanel = new Panel();
+                playerPanel.BorderStyle = BorderStyle.FixedSingle;
+                playerPanel.Size = new Size(300, 100);
+
+                int column = playerIndex / playersPerColumn;
+                int row = playerIndex % playersPerColumn;
+                playerPanel.Location = new Point(column * 310, row * 110);
+
+                TextBox playerTextBox = new TextBox();
+                playerTextBox.Size = new Size(90, 20);
+                playerTextBox.Location = new Point(10, 20);
+                playerTextBox.BorderStyle = BorderStyle.FixedSingle;
+                playerTextBox.Name = $"Player_{playerIndex}_Name";
+                playerPanel.Controls.Add(playerTextBox);
+
+                if (checkBox1.Checked)
+                {
+                    AddPlayerNumbers(playerPanel, playerIndex);
+                }
+                else if (checkBox2.Checked)
+                {
+                    AddRandomSystemNumbers(playerPanel);
+                }
+
+                panel1.Controls.Add(playerPanel);
+            }
+        }
+
+        private void AddPlayerNumbers(Panel playerPanel, int playerIndex)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                TextBox numberBox = new TextBox();
+                numberBox.Size = new Size(40, 20);
+                numberBox.Location = new Point(10 + i * 50, 50);
+                numberBox.BorderStyle = BorderStyle.FixedSingle;
+                numberBox.Tag = $"Player_{playerIndex}_Number_{i}";
+                playerPanel.Controls.Add(numberBox);
+            }
+        }
+
+        private void AddRandomSystemNumbers(Panel playerPanel)
+        {
+            HashSet<int> generatedNumbers = new HashSet<int>(); // To avoid duplicate numbers
+
+            for (int i = 0; i < 5; i++)
+            {
+                int randomNumber;
+
+                // First generate a random number
+                randomNumber = random.Next(1, 51); // Random number between 1 and 50
+
+                // Keep generating a new random number while it already exists in the HashSet
+                while (generatedNumbers.Contains(randomNumber))
+                {
+                    randomNumber = random.Next(1, 51); // Generate a new random number
+                }
+
+                generatedNumbers.Add(randomNumber); // Add the new unique number to the HashSet
+
+                Label numberLabel = new Label();
+                numberLabel.Text = randomNumber.ToString(); // Random number between 1 and 50
+                numberLabel.Size = new Size(40, 20);
+                numberLabel.TextAlign = ContentAlignment.MiddleCenter;
+                numberLabel.BorderStyle = BorderStyle.FixedSingle;
+                numberLabel.Location = new Point(10 + i * 50, 50);
+                playerPanel.Controls.Add(numberLabel);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button1Clicked == false)
+            {
+                MessageBox.Show("Please start the game first...");
+                return;
+            }
+
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("You cannot select both options. Please choose only one...");
+                return;
+            }
+            else if (!checkBox1.Checked && !checkBox2.Checked)
+            {
+                MessageBox.Show("Please select at least one game mode...");
+                return;
+            }
+
+            if (lottery.Count == 0)
+            {
+                MessageBox.Show("All numbers have been drawn from the bag. The game is over!");
+                ShowFinalResults();
+                return;
+            }
+
+            DrawLotteryNumber();
+        }
+
+        private void DrawLotteryNumber()
+        {
+            int index = random.Next(0, lottery.Count);
+            int drawnNumber = lottery[index];
+            lottery.RemoveAt(index);
+            lucky.Add(drawnNumber);
+
+            listBox1.Items.Add(drawnNumber);
+            listBox2.Items.Clear();
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            foreach (int number in lottery)
+            {
+                listBox2.Items.Add(number);
+            }
+
+            CheckPlayerMatches(drawnNumber);
+        }
+
+        private void CheckPlayerMatches(int drawnNumber)
+        {
+            foreach (Control panel in panel1.Controls)
+            {
+                if (panel is Panel playerPanel)
+                {
+                    int matchedCount = 0;
+                    int playerIndex = panel1.Controls.OfType<Panel>().ToList().IndexOf(playerPanel) + 1;
+                    TextBox nameBox = playerPanel.Controls.OfType<TextBox>().FirstOrDefault();
+                    if (nameBox != null && !playerNames.ContainsKey(playerIndex))
+                    {
+                        playerNames[playerIndex] = nameBox.Text;
+                    }
+
+                    foreach (Control control in playerPanel.Controls)
+                    {
+                        if (control is Label numberLabel)
+                        {
+                            if (int.TryParse(numberLabel.Text, out int number) && lucky.Contains(number))
+                            {
+                                matchedCount++;
+                                numberLabel.BackColor = Color.LightGreen;
+                            }
+                        }
+                        else if (control is TextBox numberBox)
+                        {
+                            if (int.TryParse(numberBox.Text, out int number) && lucky.Contains(number))
+                            {
+                                matchedCount++;
+                                numberBox.BackColor = Color.LightGreen;
+                            }
+                        }
+                    }
+
+                    playerMatches[playerIndex] = matchedCount;
+
+                    if (matchedCount == 5 && !completedPlayers.Contains(playerIndex))
+                    {
+                        completedPlayers.Add(playerIndex);
+                        MessageBox.Show($"({playerNames[playerIndex]}) matched all numbers!");
+                    }
+                }
+            }
+
+            MessageBox.Show($"Drawn number from the bag: {drawnNumber}\nRemaining numbers in the bag: {lottery.Count}");
+        }
+
+        // Show the final ranking of players
+        private void ShowFinalResults()
+        {
+            var sortedPlayers = completedPlayers
+                .OrderByDescending(playerIndex => playerMatches[playerIndex])
+                .ThenBy(playerIndex => completedPlayers.IndexOf(playerIndex))
+                .ToList();
+
+            string resultMessage = "Player Rankings:\n";
+            int rank = 1;
+            foreach (var playerIndex in sortedPlayers)
+            {
+                resultMessage += $"{rank++}. Player ({playerNames[playerIndex]}) \n";
+            }
+
+            MessageBox.Show(resultMessage, "Happy New Year !!! :) :D");
+        }
+
+        private void ResetGame()
+        {
+            // Clear all player panels
+            foreach (Control panel in panel1.Controls)
+            {
+                if (panel is Panel playerPanel)
+                {
+                    foreach (Control control in playerPanel.Controls)
+                    {
+                        if (control is Label numberLabel)
+                        {
+                            numberLabel.BackColor = Color.Transparent;
+                        }
+                        else if (control is TextBox numberBox)
+                        {
+                            numberBox.BackColor = Color.White;
+                            numberBox.Clear(); // Clear the value of TextBox
+                        }
+                    }
+                }
+            }
+
+            // Clear collections
+            lottery.Clear();
+            lucky.Clear();
+            playerMatches.Clear();
+            completedPlayers.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            playerNames.Clear(); // Clear player names
+
+            // Create new lottery bag
+            for (int i = 1; i <= 50; i++)
+            {
+                lottery.Add(i);
+            }
+
+        }
+    }
+}
